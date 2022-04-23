cf5d24b174d7ead1d777e8fac368939e8348bbe9
carrabbi <carrabbi@yahoo.com>
2022-04-23 20:34:17 +0200

    first

---
+++ zad2_chat/src/components/pages/App/App.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+
+import Button from "components/elements/button/Button";
+import InputGroup from "components/elements/input-group/InputGroup";
+import Main from "components/layouts/main/Main";
+
+import { observe, save } from "services/firebase";
+import styles from "./App.module.css";
+import { get } from "lodash";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [personInputValue, setPersonInputValue] = useState("");
+  const [messageInputValue, setMessageInputValue] = useState("");
+
+  useEffect(() => {
+    // funkcja zaawansowana
+    observe("messages/", setMessages);
+
+    get("currentUser").then((user) => {
+      if (user && user.name) {
+        setPersonInputValue(user.name);
+      }
+    });
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    save("messages/", {
+      person: personInputValue,
+      message: messageInputValue,
+    });
+
+    setPersonInputValue("");
+    setMessageInputValue("");
+  };
+
+  const handlePersonChange = (event) => {
+    setPersonInputValue(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessageInputValue(event.target.value);
+  };
+
+  return (
+    <Main>
+      <div>
+        <h1>Chat</h1>
+        <div className={styles.chatBox}>
+          <ul>
+            {messages.map((message) => (
+              <li key={message.id}>
+                <span className={styles.person}>{message.person}</span>
+                <p className={styles.message}>{message.message}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <InputGroup
+            id="person"
+            type="text"
+            label="Person"
+            handleChange={handlePersonChange}
+            inputValue={personInputValue}
+          />
+          <InputGroup
+            id="message"
+            type="text"
+            label="Message"
+            handleChange={handleMessageChange}
+            inputValue={messageInputValue}
+          />
+          {/* Napis send jest specjalnym propsem children */}
+          <Button btnType="submit">Send</Button>
+        </form>
+      </div>
+    </Main>
+  );
+}
+
+export default App;
---
+++ zad2_chat/src/components/pages/MyProfile/MyProfile.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import Main from "components/layouts/main/Main";
+import InputGroup from "components/elements/input-group/InputGroup";
+import Button from "components/elements/button/Button";
+import { save } from "services/firebase";
+import { update } from "lodash";
+
+function MyProfile() {
+  const [name, setName] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (name.length === 0) {
+      setIsError(true);
+      return;
+    }
+
+    update("currentUser/,", "name");
+
+    return (
+      <Main>
+        <h1>My profile</h1>
+        <form onSubmit={handleSubmit}>
+          <InputGroup
+            id="name"
+            type="text"
+            label="Name"
+            handleChange={handleInputChange}
+            inputValue={name}
+          />
+          {isError && <p>The field shouldnt be empty</p>}
+          <Button btnType="submit">Send</Button>
+        </form>
+      </Main>
+    );
+  };
+}
+
+export default MyProfile;
